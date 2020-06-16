9b6bddf3d39d89dba81e1f62f944e3a192a4be73
Hodaka Suzuki <altitude3190@gmail.com>
2020-06-16 16:23:52 +0900

    implement ocamlformat-vscode-extension.ocamlformat command

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,28 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { spawnSync } from "child_process";
+import path = require("path");
+
+const format = (filename: string) => {
+	const config = vscode.workspace.getConfiguration("ocamlformat-vscode-extension");
+
+	if (config.get('evalOpamEnv')) {
+		const res = spawnSync("eval $(opam env --readonly)", [], {
+			cwd: path.dirname(filename)
+		});
+	}
+
+	const ocamlformatPath = config.get('ocamlformatPath');
+	const ocamlformatOptions = (<string>config.get('ocamlformatOption')).split(',');
+	const args = ocamlformatOptions.concat(filename);
+
+	return spawnSync(ocamlformatPath, args, {
+		cwd: path.dirname(filename)
+	});
+}
+
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,9 +35,12 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ocamlformat-vscode-extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('ocamlformat-vscode-extension.ocamlformat', () => {
 		// The code you place here will be executed every time your command is executed
-
+		const { activeTextEditor } = vscode.window
+		if (!activeTextEditor) return
+		const { document } = activeTextEditor
+		const res = format(document.fileName);
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from ocamlformat-vscode-extension!');
 	});
