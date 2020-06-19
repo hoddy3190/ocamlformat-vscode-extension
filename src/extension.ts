9fa4936231620c8125c7c458d3d0f8f04a7d6a41
Hodaka Suzuki <altitude3190@gmail.com>
2020-06-19 21:27:29 +0900

    refactor

--- src/extension.ts
+++ src/extension.ts
@@ -6,33 +6,6 @@ import path = require("path");
 
 const OCAMLFORMAT_BIN_NAME = "ocamlformat";
 
-const makeOcamlformatPathUsingOpam = (dir: string) => {
-	const cmd = "eval $(opam env --readonly) > /dev/null 2>&1 && /bin/echo -n $OPAM_SWITCH_PREFIX"
-	const res = execSync(cmd, {
-		cwd: dir
-	});
-	if (res.length == 0) {
-		throw new Error(`the command outputs nothing to stdout: ${cmd}`);
-	}
-	return `${res}/bin/${OCAMLFORMAT_BIN_NAME}`;
-}
-
-const format = (filename: string) => {
-	const config = vscode.workspace.getConfiguration("ocamlformat-vscode-extension");
-
-	const ocamlformatPath = <string>config.get('customOcamlformatPath') || makeOcamlformatPathUsingOpam(path.dirname(filename));
-
-	const ocamlformatOptions = (<string>config.get('ocamlformatOption')).split(',');
-	const args = ocamlformatOptions.concat(filename);
-
-	// TODO: args uniq
-	// TODO: delete --inplace, -o, --output
-
-	return spawnSync(ocamlformatPath, args, {
-		cwd: path.dirname(filename)
-	});
-}
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -40,6 +13,33 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "ocamlformat-vscode-extension" is now active!');
+
+	const makeOcamlformatPathUsingOpam = (dir: string) => {
+		const cmd = "eval $(opam env --readonly) > /dev/null 2>&1 && /bin/echo -n $OPAM_SWITCH_PREFIX"
+		const res = execSync(cmd, {
+			cwd: dir
+		});
+		if (res.length == 0) {
+			throw new Error(`the command outputs nothing to stdout: ${cmd}`);
+		}
+		return `${res}/bin/${OCAMLFORMAT_BIN_NAME}`;
+	}
+
+	const format = (filename: string) => {
+		const config = vscode.workspace.getConfiguration("ocamlformat-vscode-extension");
+
+		const ocamlformatPath = <string>config.get('customOcamlformatPath') || makeOcamlformatPathUsingOpam(path.dirname(filename));
+
+		const ocamlformatOptions = (<string>config.get('ocamlformatOption')).split(',');
+		const args = ocamlformatOptions.concat(filename);
+
+		// TODO: args uniq
+		// TODO: delete --inplace, -o, --output
+
+		return spawnSync(ocamlformatPath, args, {
+			cwd: path.dirname(filename)
+		});
+	}
 
 	const getFullRange = (document: vscode.TextDocument) => {
 		var firstLine = document.lineAt(0);
@@ -50,33 +50,32 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ocamlformat-vscode-extension.ocamlformat', () => {
-		// The code you place here will be executed every time your command is executed
-		const { activeTextEditor } = vscode.window
-		if (!activeTextEditor) return
-		const { document } = activeTextEditor
+	context.subscriptions.push(
+		vscode.commands.registerCommand('ocamlformat-vscode-extension.ocamlformat', () => {
+			// The code you place here will be executed every time your command is executed
+			const { activeTextEditor } = vscode.window
+			if (!activeTextEditor) return
+			const { document } = activeTextEditor
 
-		try {
-			const res = format(document.fileName);
+			try {
+				const res = format(document.fileName);
 
-			if (res.error) {
-				vscode.window.showErrorMessage(res.error.message);
-			} else {
-				if (res.status == 0) {
-					const edit = new vscode.WorkspaceEdit()
-					const range = getFullRange(document)
-					edit.replace(document.uri, range, res.stdout.toString())
-					return vscode.workspace.applyEdit(edit)
+				if (res.error) {
+					vscode.window.showErrorMessage(res.error.message);
 				} else {
-					vscode.window.showErrorMessage(res.stderr.toString());
+					if (res.status == 0) {
+						const edit = new vscode.WorkspaceEdit()
+						const range = getFullRange(document)
+						edit.replace(document.uri, range, res.stdout.toString())
+						return vscode.workspace.applyEdit(edit)
+					} else {
+						vscode.window.showErrorMessage(res.stderr.toString());
+					}
 				}
+			} catch (error) {
+				vscode.window.showErrorMessage(error.message);
 			}
-		} catch (error) {
-			vscode.window.showErrorMessage(error.message);
-		}
-	});
-
-	context.subscriptions.push(disposable);
+		}));
 
 
 	context.subscriptions.push(
