ccc11b9ff9e0fe61159e6467eb50208a4f379d0f
Hodaka Suzuki <altitude3190@gmail.com>
2020-06-21 15:08:17 +0900

    delete comments made by yo code in extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,10 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import * as path from "path";
 import * as cmd from "./cmd";
 
 const OCAMLFORMAT_BIN_NAME = "ocamlformat";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log(
     'Congratulations, your extension "ocamlformat-vscode-extension" is now active!'
   );
@@ -54,14 +48,10 @@ export function activate(context: vscode.ExtensionContext) {
     return new vscode.Range(firstLine.range.start, lastLine.range.end);
   };
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   context.subscriptions.push(
     vscode.commands.registerCommand(
       "ocamlformat-vscode-extension.ocamlformat",
       () => {
-        // The code you place here will be executed every time your command is executed
         const { activeTextEditor } = vscode.window;
         if (!activeTextEditor) {
           return;
